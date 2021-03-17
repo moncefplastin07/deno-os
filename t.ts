0ac692ddea91ffc18ea037bf327528d192ed049e
Moncef Gaha <moncefplastin07@gmail.com>
2021-03-17 07:56:49 +0100

    update t.ts

--- t.ts
+++ t.ts
@@ -1,3 +1,6 @@
-import os from "./mod.ts";
+import os from "https://deno.land/x/dos@v0.1.2/mod.ts";
 
 console.log(os.platform());
+console.log(await os.arch());
+console.log(os.tempDir());
+console.log(os.homeDir());
